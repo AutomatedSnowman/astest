1f4152e3dfffffa88a7ed5290d8ffd2c9a45f51a
Chetan Tiwari <chetantiw@gmail.com>
2025-05-03 08:08:06 +0000

    Thingsboard custimzation: login logo, main logo, favicon, tab name, colour - all done successfully

--- ui-ngx/src/environments/environment.prod.ts
+++ ui-ngx/src/environments/environment.prod.ts
@@ -15,7 +15,7 @@
 ///
 
 export const environment = {
-  appTitle: 'ThingsBoard',
+  appTitle: 'MuTech-IoT',
   production: true,
 // @ts-ignore
   tbVersion: TB_VERSION,
--- ui-ngx/src/environments/environment.ts
+++ ui-ngx/src/environments/environment.ts
@@ -19,7 +19,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  appTitle: 'ThingsBoard',
+  appTitle: 'MuTech-IoT',
   production: false,
 // @ts-ignore
   tbVersion: TB_VERSION,
