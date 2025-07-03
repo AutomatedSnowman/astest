a88b8bdc66be37a48fa60073d82ae1cb165c44e5
AutomatedSnowman <157937054+AutomatedSnowman@users.noreply.github.com>
2025-07-03 10:10:01 -0400

    Update environment.ts

--- ui-ngx/src/environments/environment.ts
+++ ui-ngx/src/environments/environment.ts
@@ -19,7 +19,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  appTitle: 'MuTech-IoT',
+  appTitle: 'AutoSnow Dashboard',
   production: false,
 // @ts-ignore
   tbVersion: TB_VERSION,
